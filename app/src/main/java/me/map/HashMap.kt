bfbd95754bcdfbfef5601892cecfcd2df339b421
Александр Кундрюков <a.kundryukov@gmail.com>
2025-03-14 00:30:05 +0300

    Update HashMap;

---
+++ app/src/main/java/me/map/HashMap.kt
@@ -0,0 +1,49 @@
+package me.map
+
+import java.util.LinkedList
+
+class KeyValue<T>(val key:Any, var value:T)
+class HashMap<T>(val bucketsCount: Int = 1024*1024) {
+    private val buckets = mutableListOf<LinkedList<KeyValue<T>>>().apply {
+        repeat(bucketsCount) {
+            add(LinkedList<KeyValue<T>>())
+        }
+    }
+
+    fun put(key: Any, value: T) {
+        val hashCode = hashOf(key)
+        val bucketIndex = hashCode.mod(buckets.size)
+
+        val bucket = buckets[bucketIndex]
+        for(i in 0 until bucket.size) {
+            if(bucket[i].key == key) {
+                bucket[i].value = value
+                return
+            }
+        }
+
+        // else
+        bucket.add(KeyValue(key, value))
+    }
+
+    fun remove(key: Any) {
+        val hashCode = hashOf(key)
+        val bucketIndex = hashCode.mod(buckets.size)
+        buckets[bucketIndex].removeIf { it.key == key }
+    }
+
+    operator fun set(key: Any, value: T) {
+        put(key, value)
+    }
+
+    operator fun get(key: Any): T? {
+        val hashCode = hashOf(key)
+        val bucketIndex = hashCode.mod(buckets.size)
+
+        return buckets[bucketIndex].firstOrNull{ it.key == key }?.value
+    }
+
+    private fun hashOf(data:Any):Int {
+        return data.hashCode() // todo: add my own implementation
+    }
+}
---
+++ app/src/test/java/me/map/HashMapTest.kt
@@ -0,0 +1,105 @@
+package me.map
+
+import org.junit.Test
+import kotlin.collections.set
+
+
+class HashMapTest {
+
+    @Test
+    fun addStringKey() {
+        val map = HashMap<String>()
+        map.put("Key", "Value1")
+        assert(map["Key"] == "Value1")
+    }
+
+    @Test
+    fun setStringKey() {
+        val map = HashMap<String>()
+        map.put("Key", "Value1")
+        map["Key"] = "Value2"
+
+        assert(map["Key"] == "Value2")
+    }
+
+    @Test
+    fun removeStringKey() {
+        val map = HashMap<String>()
+        map.put("Key", "Value1")
+        map.remove("Key")
+
+        assert(map["Key"] == null)
+    }
+
+    @Test
+    fun addAnyKey() {
+        val map = HashMap<String>()
+        map.put(Unit, "Value1")
+        assert(map[Unit] == "Value1")
+    }
+
+    @Test
+    fun setAnyKey() {
+        val map = HashMap<String>()
+        map.put(Unit, "Value1")
+        map[Unit] = "Value2"
+        assert(map[Unit] == "Value2")
+    }
+
+    @Test
+    fun removeAnyKey() {
+        val map = HashMap<String>()
+        map.put(Unit, "Value1")
+        map.remove(Unit)
+
+        assert(map[Unit] == null)
+    }
+
+    @Test
+    fun productivityHashMap() { // 1 401 ms, 2 954 ms
+        val start = System.currentTimeMillis()
+        val map = kotlin.collections.HashMap<String, String>()
+        repeat(1024*1024) {
+            map.put("Key$it", "Value$it")
+        }
+        map["Key5"] = "Value5Edited"
+        map["Key6"] = "Value6Edited"
+        map["Key9"] = "Value9Edited"
+        map["Key7"] = "Value7Edited"
+        map.remove("Key100")
+        map.remove("Key1005")
+        map.remove("Key100500")
+        map["Key3"] = "Value3Edited"
+        println(map["Key5"])
+        val finish = System.currentTimeMillis()
+        println("time: ${finish-start}Ms")
+    }
+
+    @Test
+    fun productivityNewMap() { // 1 771 ms, 2 762 ms
+        val start = System.currentTimeMillis()
+        val map = HashMap<String>()
+        repeat(1024*1024) {
+            map.put("Key$it", "Value$it")
+           // println("i: $it")
+        }
+        map["Key5"] = "Value5Edited"
+        map["Key6"] = "Value6Edited"
+        map["Key9"] = "Value9Edited"
+        map["Key7"] = "Value7Edited"
+        map.remove("Key100")
+        map.remove("Key1005")
+        map.remove("Key100500")
+        map["Key3"] = "Value3Edited"
+        println(map["Key5"])
+        val finish = System.currentTimeMillis()
+        println("time: ${finish-start}Ms")
+    }
+
+    // NOTE: То как принято говорить о реализации HashMap на собеседованиях,
+    // без дополнительных оптимизаций работает ОЧЕНЬ медленно на больших объемах данных,
+    // судя по всему тут поможет добавление новых баккетов
+    // при возрастании кол-ва входящих данных
+
+    // да, проблема решается если установить кол-во баккетов соответствующее кол-ву данных
+}
